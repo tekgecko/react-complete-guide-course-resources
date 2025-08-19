153c32ec1a61f13b048f3a60b2d9def509c49721
Sam Mansfield <sam.mansfield@hotmail.co.uk>
2025-08-19 13:11:52 +0100

    Added own code

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import { useState } from "react";
+
 function App() {
+  
+  let titles = ["React Basics", "State and Lifecycle", "Events", "Conditional Rendering", "Lists"]
+  const [title, setTitle] = useState("React Essentials");
+  function handleClick() {
+    setTitle(titles[Math.floor(Math.random() * titles.length)] || "React Essentials");
+  }
+
   return (
     <div>
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
+        <h1>{title}</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
           going to build!
@@ -11,6 +21,7 @@ function App() {
       </header>
       <main>
         <h2>Time to get started!</h2>
+        <button onClick={handleClick}>Hello</button>
       </main>
     </div>
   );
